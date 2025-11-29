53b2d0d49516fb2d49565794bcc9d935eb9459ce
liuzibo <liuzibo1925@outlook.com>
2025-11-29 15:53:45 +0800

    init

---
+++ src/main/java/com/zeber/P0/Main.java
@@ -0,0 +1,8 @@
+package com.zeber.P0;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.hello();
+    }
+}
---
+++ src/main/java/com/zeber/P0/Solution.java
@@ -0,0 +1,7 @@
+package com.zeber.P0;
+
+public class Solution {
+    public void hello() {
+        System.out.println("Hello World");
+    }
+}
